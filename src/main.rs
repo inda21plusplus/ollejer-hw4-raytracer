61e84b5421117cfcf6c4822f525663cc279987a6
Olle Jernström WSL <ollejer@ug.kth.se>
2021-10-10 00:29:01 +0200

    Add gradiant background and sphere with shading

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,22 @@ mod ray;
 use ray::Ray;
 use macaw::Vec3;
 
+
 fn ray_color(ray: &Ray) -> Vec3 {
+    let sphere_center = Vec3::new(0.0, 0.0,-1.0);
+    let t = hit_sphere(sphere_center, 0.5, ray);
+    if t > 0.0 {
+        let n = ray.at(t) - sphere_center;
+        let n = n.normalize();
+
+        let color = Vec3::new(n.x + 1., n.y + 1., n.z + 1.);
+        return 0.5*color
+    }
+
     let unit_direction = ray.clone().direction().normalize();
     let t = 0.5 * (unit_direction.y + 1.0);
     let color1 = Vec3::new(1.0, 1.0, 1.0);
     let color2 = Vec3::new(0.5, 0.7, 1.0);
-
     (1.0 - t) * color1 + t * color2
 }
 
@@ -21,6 +31,24 @@ fn write_color(pixel_color: Vec3) {
         (pixel_color.z * 255.999) as u8
     );
 }
+
+fn hit_sphere(center: Vec3, radius: f32, ray: &Ray) -> f32 {
+    let oc = ray.origin() - center; 
+    let ray_dir = ray.direction();
+
+    let a = ray_dir.length_squared();
+    let half_b = oc.dot(ray_dir);
+    let c = oc.length_squared() - radius*radius;
+    let discriminant = half_b*half_b - a*c;
+
+    if discriminant < 0.0 {
+        -1.0 
+    } else {
+        (-half_b - discriminant.sqrt()) / a
+    }
+}
+
+
 
 fn main() {
 
